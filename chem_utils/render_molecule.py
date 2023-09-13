7a0e0159e08d17b9555f9cdfc5996769c232695c
ivan <imtambovtcev@gmail.com>
2023-09-13 01:50:37 +0000

    + basis_vectors for render moleceule

--- chem_utils/render_molecule.py
+++ chem_utils/render_molecule.py
@@ -21,50 +21,58 @@ default_atoms_settings['Color'] = [[int(i) for i in s.replace(
     '[', '').replace(']', '').split(',')] for s in default_atoms_settings['Color']]
 
 
-def render_molecule(plotter: pv.Plotter, atoms: ase.Atoms, atoms_settings=None, show_hydrogens=True, alpha=1.0, atom_numbers=False, show_hydrogen_bonds=False, show_numbers=False):
-
+def render_molecule(plotter: pv.Plotter, atoms: ase.Atoms, atoms_settings=None, show_hydrogens=True, alpha=1.0, atom_numbers=False, show_hydrogen_bonds=False, show_numbers=False, show_basis_vectors=True):
     if atoms_settings is None:
         atoms_settings = default_atoms_settings
 
+    # Get unique symbols from atoms
     symb = list(set(atoms.symbols))
     _atoms_settings = atoms_settings.loc[symb]
     atom_positions = atoms.positions
     atom_symbols = atoms.get_chemical_symbols()
 
     for atom, position, symbol in zip(atoms, atom_positions, atom_symbols):
-        symbol = symbol if symbol in _atoms_settings.index else 'Unknown'
+        # Ensure symbol is in the DataFrame's index
+        if symbol in _atoms_settings.index:
+            settings = _atoms_settings.loc[symbol]
+        else:
+            print(f"Warning: {symbol} not found in settings. Using default.")
+            settings = _atoms_settings.loc['Unknown']
+        
         if show_hydrogens or symbol != 'H':
             sphere = pv.Sphere(
-                radius=_atoms_settings.loc[symbol]['Radius'], center=position, theta_resolution=12, phi_resolution=12)
+                radius=settings['Radius'], center=position, theta_resolution=12, phi_resolution=12)
             plotter.add_mesh(
-                sphere, color=_atoms_settings.loc[symbol]['Color'], smooth_shading=True, opacity=alpha)
+                sphere, color=settings['Color'], smooth_shading=True, opacity=alpha)
 
+    # Display atom numbers if required
     if atom_numbers:
         poly = pv.PolyData(atom_positions)
         poly["My Labels"] = [str(i) for i in range(poly.n_points)]
         plotter.add_point_labels(
             poly, "My Labels", point_size=20, font_size=36)
 
+    # Generate bond pairs
     pairs = [(a, b) for idx, a in enumerate(symb) for b in symb[idx + 1:]]+[(i, i)
                                                                             for i in symb] if len(symb) > 1 else [(symb[0], symb[0])]
     ana = Analysis(atoms)
 
+    # Display hydrogen bonds if required
     if show_hydrogens and show_hydrogen_bonds:
-        h_coords = atom_positions[np.array(atom_symbols) == 'H']
-        f_coords = atom_positions[np.array(atom_symbols) == 'F']
+        h_indices = np.where(np.array(atom_symbols) == 'H')[0]
+        f_indices = np.where(np.array(atom_symbols) == 'F')[0]
+        for h_index in h_indices:
+            h_coord = atom_positions[h_index]
+            for f_index in f_indices:
+                f_coord = atom_positions[f_index]
+                if np.linalg.norm(h_coord - f_coord) < 2.2:
+                    line = pv.Line(h_coord, f_coord)
+                    plotter.add_mesh(line, color='white', opacity=alpha, line_width=2)
 
-        # Compute the distance matrix
-        distance_matrix = np.linalg.norm(
-            h_coords[:, None, :] - f_coords[None, :, :], axis=-1)
-        # Iterate over the pairs within distance threshold
-        for h_coord, f_coord in zip(h_coords[distance_matrix < 2.2], f_coords[distance_matrix < 2.2]):
-            line = pv.Line(h_coord, f_coord)
-            plotter.add_mesh(line, color='white', opacity=alpha, line_width=2)
-
+    # Render bonds
     for bond_type in pairs:
         if show_hydrogens or not ('H' in bond_type):
             bonds = ana.get_bonds(*bond_type, unique=True)[0]
-
             for bond in bonds:
                 atom_a = atom_positions[bond[0]]
                 atom_b = atom_positions[bond[1]]
@@ -87,35 +95,42 @@ def render_molecule(plotter: pv.Plotter, atoms: ase.Atoms, atoms_settings=None, 
                 plotter.add_mesh(cylinder, color='#D3D3D3',
                                  smooth_shading=True, opacity=alpha)
 
+    if show_basis_vectors:
+        origin = np.array([0, 0, 0])
+        
+        basis_vectors = [np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 1])]
+        colors = ['red', 'green', 'blue']
+
+        for direction, color in zip(basis_vectors, colors):
+            arrow = pv.Arrow(start=origin, direction=direction, shaft_radius=0.05, tip_radius=0.1)
+            plotter.add_mesh(arrow, color=color)
+
+    # Display atom IDs if required
     if show_numbers:
         poly = pv.PolyData(atom_positions)
-        poly["Atom IDs"] = [str(atom.index)
-                            for atom in atoms]  # using atom's index as its ID
+        poly["Atom IDs"] = [str(atom.index) for atom in atoms]
         plotter.add_point_labels(
             poly, "Atom IDs", point_size=20, font_size=36, render_points_as_spheres=False)
 
-    # Hotkey function to print camera position and copy to clipboard
+    # Hotkey functions
     def copy_camera_position_to_clipboard():
         cam_pos = plotter.camera_position
         cam_pos_str = f"Camera Position: {cam_pos}"
         print(cam_pos_str)
-
-        # Copy to clipboard
         pyperclip.copy(cam_pos_str)
 
-    # Hotkey function to save the render view as an image using a "Save As" dialog
     def save_render_view_with_dialog():
         root = tk.Tk()
-        root.withdraw()  # Hide the root window
+        root.withdraw()
         file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[
                                                  ("PNG files", "*.png"), ("All files", "*.*")])
         if file_path:
             plotter.screenshot(file_path)
             print(f"Saved image as '{file_path}'.")
 
-    # Add hotkeys to the plotter
     plotter.add_key_event("c", copy_camera_position_to_clipboard)
     plotter.add_key_event("p", save_render_view_with_dialog)
+
 
 
 def render_molecule_from_atoms(atoms, plotter=None, save=None, cpos=None, atoms_settings=default_atoms_settings, alpha=1.0, notebook=False, auto_close=True, interactive=False, background_color='black', show_hydrogen_bonds=False, show_numbers=False):
