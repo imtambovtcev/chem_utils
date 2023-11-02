60dce97b25ebab862c393b9b0abb109a74706cd5
ivan <imtambovtcev@gmail.com>
2023-11-02 11:46:28 +0000

    + second way of getting freq

--- chem_utils/frequency.py
+++ chem_utils/frequency.py
@@ -1,5 +1,7 @@
 import cclib
 import numpy as np
+import re
+import pandas as pd
 
 
 class Frequency:
@@ -9,10 +11,45 @@ class Frequency:
 
     @classmethod
     def load(cls, filename):
-        data = cclib.io.ccread(filename)
-        return cls(data.vibfreqs, data.vibdisps)
+        try:
+            data = cclib.io.ccread(filename)
+            vibfreqs = data.vibfreqs
+            vibdisps = data.vibdisps
+        except:
+            numeric_const_pattern = '[-+]? (?: (?: \d* \. \d+ ) | (?: \d+ \.? ) )(?: [Ee] [+-]? \d+ ) ?'
+            rx = re.compile(numeric_const_pattern, re.VERBOSE)
+            data = pd.DataFrame()
+            with open(filename) as f:
+                lines = f.readlines()
+                file_key = 'error'
+                for id, line in enumerate(lines[::-1]):
+                    if 'VIBRATIONAL FREQUENCIES' in line:
+                        file_key = 'TS'
+                        break
+                id = len(lines) - id
+                # print(f'{id = }')
+                # print(f'{line = }')
+                # print(f'{file_key = }')
+
+                for i, line in enumerate(lines[id + 4:]):
+                    # print(f'{line = }')
+                    if line.startswith('--------') or len(line) <= 2:
+                        break
+                    if file_key == 'TS':
+                        d = rx.findall(line)
+                        if 'imaginary mode' in line:
+                            comment = 'imaginary mode'
+                        else:
+                            comment = ''
+                        data = pd.concat([data, pd.DataFrame(
+                            {'Frequency, cm**-1': [float(d[1])], 'Comment': [comment]})], ignore_index=True)
+                # cclib failed
+            data = data[data['Frequency, cm**-1'] != 0]
+            vibfreqs = data['Frequency, cm**-1']
+            vibdisps = None
+
+        return cls(vibfreqs, vibdisps)
 
     @property
     def is_minimum(self):
         return np.all(self.frequencies > 0)
-
