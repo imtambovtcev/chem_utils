7d282a9e136996d140389b26df753fd4fea72af3
ivan <imtambovtcev@gmail.com>
2024-11-02 00:15:02 +0000

    + fragment vector render update

--- chem_utils/fragment.py
+++ chem_utils/fragment.py
@@ -101,10 +101,26 @@ class Fragment(Molecule):
         # Convert the reordered molecule to a Fragment instance and return
         return Fragment(reordered_molecule, attach_atom=new_attach_atom, attach_matrix=self.attach_matrix)
 
-    def fragment_vectors_render(self, plotter: pv.Plotter = None, notebook=False):
+    def main_vectors_render(self, plotter: pv.Plotter = None, notebook=False):
         if plotter is None:
             plotter = pv.Plotter(notebook=notebook)
         V0, V1, V2, V3 = self.fragment_vectors
+        add_vector_to_plotter(plotter, V0, V1, color='red', scale_factor=1.0)
+        add_vector_to_plotter(plotter, V0, V2, color='green', scale_factor=0.7)
+        add_vector_to_plotter(plotter, V0, V3, color='blue', scale_factor=0.7)
+        return plotter
+
+    def get_main_vectors(self):
+        V0 = self.attach_point
+        V1 = self.attach_matrix[:, 0]
+        V2 = self.attach_matrix[:, 1]
+        V3 = self.attach_matrix[:, 2]
+        return V0, V1, V2, V3
+
+    def main_vectors_render(self, plotter: pv.Plotter = None, notebook=False):
+        if plotter is None:
+            plotter = pv.Plotter(notebook=notebook)
+        V0, V1, V2, V3 = self.get_main_vectors()
         add_vector_to_plotter(plotter, V0, V1, color='red', scale_factor=1.0)
         add_vector_to_plotter(plotter, V0, V2, color='green', scale_factor=0.7)
         add_vector_to_plotter(plotter, V0, V3, color='blue', scale_factor=0.7)
@@ -121,8 +137,7 @@ class Fragment(Molecule):
         self.attach_matrix = np.dot(R, self.attach_matrix)
 
     def get_origin_rotation_matrix(self):
-        V0, V1, V2, V3 = self.fragment_vectors
-        return np.linalg.inv(np.column_stack([V1, V2, V3]))
+        return np.linalg.inv(self.attach_matrix)
 
     def set_to_origin(self):
         self.apply_transition(-self.attach_point)
@@ -130,10 +145,10 @@ class Fragment(Molecule):
 
     def render(self, **kwargs):
         if 'plotter' in kwargs:
-            plotter = self.fragment_vectors_render(kwargs['plotter'])
+            plotter = self.main_vectors_render(kwargs['plotter'])
         else:
             notebook = kwargs.get('Notebook', False)
-            plotter = self.fragment_vectors_render(None, notebook=notebook)
+            plotter = self.main_vectors_render(None, notebook=notebook)
 
         # Update the plotter in kwargs before calling super
         kwargs['plotter'] = plotter
@@ -143,8 +158,8 @@ class Fragment(Molecule):
         # Get the plotter from kwargs or default to None
         plotter = kwargs.get('plotter', None)
 
-        # Process the plotter using fragment_vectors_render
-        plotter = self.fragment_vectors_render(plotter)
+        # Process the plotter using main_vectors_render
+        plotter = self.main_vectors_render(plotter)
 
         # Update the plotter in kwargs
         kwargs['plotter'] = plotter
@@ -178,9 +193,9 @@ class Fragment(Molecule):
             scale_factor = 0.01
             while len(molecule.get_all_bonds()) != len(self.get_all_bonds()) + len(_fragment.get_all_bonds()):
                 print(
-                    f'{len(molecule.get_all_bonds()) = } {len(self.get_all_bonds()) + len(_fragment.get_all_bonds()) = }')
+                    f'{len(molecule.get_all_bonds())=} {len(self.get_all_bonds()) + len(_fragment.get_all_bonds())=}')
                 scale_factor += 0.01
-                print(f'{scale_factor = }')
+                print(f'{scale_factor=}')
                 _fragment = fragment.copy()
                 _fragment.set_to_origin()
                 random_matrix = np.random.rand(3, 3) * scale_factor
